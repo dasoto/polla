ec1dc8658244e172ee860c645f302abacacefe6a
David Soto Mora <davidsotomora@google.com>
2024-06-26 19:35:58 -0400

    Fix

--- README.md
+++ README.md
@@ -8,17 +8,17 @@ This is a repo with a piece of code to keep track of the fantasy game with my fr
 
 | Position | Nombre | Score |
 | -------- | ------ | ----- |
-|1. | ./predictions/Lucas | 14 |
-|2. | ./predictions/Angelo | 14 |
-|3. | ./predictions/Wabers | 13 |
-|4. | ./predictions/Black | 12 |
-|5. | ./predictions/Criss | 12 |
-|6. | ./predictions/David | 10 |
-|7. | ./predictions/Roberto | 8 |
-|8. | ./predictions/Andyk | 8 |
-|9. | ./predictions/Maigus | 7 |
-|10. | ./predictions/Kevin | 7 |
-|11. | ./predictions/Samuel | 7 |
-|12. | ./predictions/Gabito | 6 |
-|13. | ./predictions/Lawrence | 5 |
-|14. | ./predictions/Karla | 4 |
+|1. | Lucas | 14 |
+|2. | Angelo | 14 |
+|3. | Wabers | 13 |
+|4. | Black | 12 |
+|5. | Criss | 12 |
+|6. | David | 10 |
+|7. | Roberto | 8 |
+|8. | Andyk | 8 |
+|9. | Maigus | 7 |
+|10. | Kevin | 7 |
+|11. | Samuel | 7 |
+|12. | Gabito | 6 |
+|13. | Lawrence | 5 |
+|14. | Karla | 4 |
--- polla.py
+++ polla.py
@@ -1,18 +1,17 @@
-
 from typing import List
 import pandas as pd
 from glob import glob
 from score_updates import get_latest_scores
 
 
-
 class Polla:
     def __init__(
-            self, 
-            master_file: str = 'master_plan.csv', 
-            finalists_file: str = 'finalists.csv', 
-            predictions_files: str = './predictions/*.txt', 
-            scoreboard_file = 'scoreboard.csv'):
+        self,
+        master_file: str = "master_plan.csv",
+        finalists_file: str = "finalists.csv",
+        predictions_files: str = "./predictions/*.txt",
+        scoreboard_file="scoreboard.csv",
+    ):
         self.master_file = master_file
         self.finalists_file = finalists_file
         self.predictions_files = predictions_files
@@ -20,22 +19,22 @@ class Polla:
         self.master_plan, self.finalists = self.load()
         self.participants = []
         self.load_predictions()
-    
+
     def load_predictions(self):
         for file in glob(self.predictions_files):
             data, last_four = self._parser_file(file)
-            participant_name = file.split('\\')[-1].split('.txt')[0]
+            participant_name = file.split("\\")[-1].split(".txt")[0]
+            if "./predictions/" in participant_name:
+                participant_name = participant_name.split("./predictions/")[-1]
             self.participants.append(participant_name)
             self.master_plan[participant_name] = data
             self.finalists[participant_name] = last_four
-        
 
     def load(self):
-        master = pd.read_csv(self.master_file, index_col='Match')
-        finalists = pd.read_csv(self.finalists_file, index_col='Position')
+        master = pd.read_csv(self.master_file, index_col="Match")
+        finalists = pd.read_csv(self.finalists_file, index_col="Position")
         return master, finalists
 
-    
     def _parser_file(self, filename) -> List:
         with open(filename) as f:
             data = f.readlines()
@@ -49,59 +48,78 @@ class Polla:
             if i < 24:
                 all_data_clean.append(line)
             elif (
-                (i >= 54 and i < 58) or
-                (i >= 66 and i < 68) or
-                (i >= 78 and i < 79) or
-                (i >= 81 and i < 82)):
+                (i >= 54 and i < 58)
+                or (i >= 66 and i < 68)
+                or (i >= 78 and i < 79)
+                or (i >= 81 and i < 82)
+            ):
                 all_data_clean.append(line[-5:])
-            elif (i >= 84 and i < 88):
+            elif i >= 84 and i < 88:
                 last_four.append(line)
-            
+
             i += 1
         return all_data_clean, last_four
 
     def update_goals(self):
         latest_scores = get_latest_scores(simulate=False)
-        print(f'Latest Scores: {latest_scores}')
+        print(f"Latest Scores: {latest_scores}")
         for result in latest_scores:
             local_team, visit_team, _ = result.keys()
             goal_local, goal_visit, status = result.values()
-            index_match = self.master_plan[(self.master_plan.Status != 'jugado') & (self.master_plan.Local == local_team) & (polla.master_plan.Visita == visit_team)].index
-            self.master_plan.loc[index_match, 'goal_local'] = goal_local
-            self.master_plan.loc[index_match, 'goal_visita'] = goal_visit
-            self.master_plan.loc[index_match, 'Status'] = status            
+            index_match = self.master_plan[
+                (self.master_plan.Status != "jugado")
+                & (self.master_plan.Local == local_team)
+                & (polla.master_plan.Visita == visit_team)
+            ].index
+            self.master_plan.loc[index_match, "goal_local"] = goal_local
+            self.master_plan.loc[index_match, "goal_visita"] = goal_visit
+            self.master_plan.loc[index_match, "Status"] = status
 
     def _get_tendencia(self, row):
-        if row['Status'] == 'pendiente':
-            return '-'
-        if row['goal_local'] == row['goal_visita']:
+        if row["Status"] == "pendiente":
+            return "-"
+        if row["goal_local"] == row["goal_visita"]:
             return f"X|{int(row['goal_local'])}-{int(row['goal_visita'])}"
-        elif row['goal_local'] > row['goal_visita']:
+        elif row["goal_local"] > row["goal_visita"]:
             return f"1|{int(row['goal_local'])}-{int(row['goal_visita'])}"
         else:
             return f"2|{int(row['goal_local'])}-{int(row['goal_visita'])}"
 
-
     def compute_results(self):
-        self.master_plan['results'] = self.master_plan.apply(lambda row: self._get_tendencia(row), axis=1)
-
+        self.master_plan["results"] = self.master_plan.apply(
+            lambda row: self._get_tendencia(row), axis=1
+        )
 
     def save(self):
         self.master_plan.to_csv(self.master_file)
         self.finalists.to_csv(self.finalists_file)
         self.scoreboard.to_csv(self.scoreboard_file)
-        print('All saved!')
-
+        print("All saved!")
 
     def build_scoreboard(self):
-        scoreboard = {k: self._get_points(k, self.master_plan) + self._get_points_finalists(k, self.finalists) for k in self.participants}
-        scoreboard =  {k: v for k, v in sorted(scoreboard.items(), key=lambda item: item[1], reverse=True)}
-        self.scoreboard = pd.DataFrame({'Position': [ x for x in range(1,len(self.participants)+1)], 'Participante':scoreboard.keys(), 'Points': scoreboard.values() }).set_index('Position')
+        scoreboard = {
+            k: self._get_points(k, self.master_plan)
+            + self._get_points_finalists(k, self.finalists)
+            for k in self.participants
+        }
+        scoreboard = {
+            k: v
+            for k, v in sorted(
+                scoreboard.items(), key=lambda item: item[1], reverse=True
+            )
+        }
+        self.scoreboard = pd.DataFrame(
+            {
+                "Position": [x for x in range(1, len(self.participants) + 1)],
+                "Participante": scoreboard.keys(),
+                "Points": scoreboard.values(),
+            }
+        ).set_index("Position")
         return self.scoreboard
 
     def _get_points_finalists(self, participant, df):
         total_score = 0
-        for index, row in df[df.Status != 'pendiente'].iterrows():
+        for index, row in df[df.Status != "pendiente"].iterrows():
             if row.Results == row[participant]:
                 total_score += 3
             elif row[participant] in df.Results:
@@ -110,33 +128,32 @@ class Polla:
 
     def _get_points(self, participant, df):
         total_score = 0
-        for index, row in df[df.Status != 'pendiente'].iterrows():
+        for index, row in df[df.Status != "pendiente"].iterrows():
             if row.results == row[participant]:
                 total_score += 3
             elif row.results[0:1] == row[participant][0:1]:
                 total_score += 1
         return total_score
-    
+
     def _write_scoreboard(self):
         output_markdown = [
-            '# Copa America 2024\n',
-            '\n',
-            'This is a repo with a piece of code to keep track of the fantasy game with my friends',
-            ' for the Copa America 2024.\n\n',
-            '## Predicciones y resultados parciales\n',
-            '- [Pronosticos y resultados](https://github.com/dasoto/polla/blob/main/master_plan.csv)\n'
-            '## Scoreboard\n\n'
-            '| Position | Nombre | Score |\n',
-            '| -------- | ------ | ----- |\n',
-            
-            ]
+            "# Copa America 2024\n",
+            "\n",
+            "This is a repo with a piece of code to keep track of the fantasy game with my friends",
+            " for the Copa America 2024.\n\n",
+            "## Predicciones y resultados parciales\n",
+            "- [Pronosticos y resultados](https://github.com/dasoto/polla/blob/main/master_plan.csv)\n"
+            "## Scoreboard\n\n"
+            "| Position | Nombre | Score |\n",
+            "| -------- | ------ | ----- |\n",
+        ]
         for index, row in self.scoreboard.iterrows():
-            output_markdown.append(f'|{index}. | {row.Participante} | {row.Points} |\n')
-        with open('README.md', 'w') as md:
+            output_markdown.append(f"|{index}. | {row.Participante} | {row.Points} |\n")
+        with open("README.md", "w") as md:
             md.writelines(output_markdown)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     polla = Polla()
     polla.update_goals()
     polla.compute_results()
@@ -145,8 +162,6 @@ if __name__ == '__main__':
     polla.save()
     print(polla.master_plan)
     polla._write_scoreboard()
-
-
 
 
 # group_phase = clean_data[0:24]
